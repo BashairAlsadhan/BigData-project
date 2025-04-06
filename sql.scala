5996c66247e84198b4389e7ba0e6d3fafe5c18be
ramafahad <52184019+ramafahad@users.noreply.github.com>
2025-04-06 23:29:22 +0900

    add query4 and 5

--- sql.scala
+++ sql.scala
@@ -76,6 +76,46 @@ ORDER BY AVG_Teacher_Student_Relationship
 val result_3 = spark.sql(query_3)
 
 
+val query_4 =
+"""
+SELECT 
+    CASE
+        WHEN social_support_from_friends <= 0.2 THEN 'Very Low Support'
+        WHEN social_support_from_friends <= 0.4 THEN 'Low Support'
+        WHEN social_support_from_friends <= 0.6 THEN 'Moderate Support'
+        WHEN social_support_from_friends <= 0.8 THEN 'High Support'
+        WHEN social_support_from_friends <= 1 THEN 'Very High Support'
+    END AS Friend_Support_Level,
+    round(avg(stress_level), 2) AS AVG_Stress,
+    round(avg(academic_performance), 2) AS AVG_Performance,
+    round(avg(mental_health_issues), 2) AS AVG_Mental_Health_Issues
+FROM stressDF
+GROUP BY Friend_Support_Level
+ORDER BY AVG_Stress
+"""
+val result_4 = spark.sql(query_4)
+
+
+val query_5 =
+"""
+SELECT 
+    CASE
+        WHEN sleep_quality <= 0.2 THEN 'Very Poor Sleep'
+        WHEN sleep_quality <= 0.4 THEN 'Poor Sleep'
+        WHEN sleep_quality <= 0.6 THEN 'Moderate Sleep'
+        WHEN sleep_quality <= 0.8 THEN 'Good Sleep'
+        WHEN sleep_quality <= 1 THEN 'Excellent Sleep'
+    END AS Sleep_Quality_Level,
+    round(avg(stress_level), 2) AS AVG_Stress,
+    round(avg(academic_performance), 2) AS AVG_Performance,
+    round(avg(mental_health_issues), 2) AS AVG_Mental_Health_Issues
+FROM stressDF
+GROUP BY Sleep_Quality_Level
+ORDER BY AVG_Stress
+"""
+val result_5 = spark.sql(query_5)
+
+
 // Query1:
 println("How are the activity levels affecting the student performance?")
 result_1.show()
@@ -85,6 +125,14 @@ println("How are future career concerns affecting Stress Levels for students?")
 result_2.show()
 
 // Query3:
-println("Is Academic Performance related with study load and teacher_student_relationship ?")
+println("Is Academic Performance related with study load and teacher_student_relationship?")
 result_3.show()
+
+// Query4:
+println("Does Social Support from Friends Help Reduce Stress?")
+result_4.show()
+
+// Query3:
+println("How Does Sleep Quality Influence Stress and Academic Outcomes?")
+result_5.show()
 
